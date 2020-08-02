ad929ec51cd5d34860e63758cc61277e72584055
Alamo_Liu <alamo_liuyang@foxmail.com>
2020-08-02 18:54:10 +0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,6 @@
 动态规划解法总结
 """
 
-
 # maxProfit_{}: {}中是交易次数
 
 def maxProfit_1(prices):
@@ -47,7 +46,62 @@ print(maxProfit_1_dp([7,1,5,3,6,4]))
 def maxProfit_n(prices):
     """
     可进行多次交易, 不限交易次数
-    
+    等价于每天都进行买卖, 选择利润为正的进行交易
     :param prices:
     :return:
     """
+    max_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i]>prices[i-1]:
+            max_profit += prices[i]-prices[i-1]
+    return max_profit
+print(maxProfit_n([7,1,5,3,6,4]))
+
+def maxProfit_n_dp(prices):
+    """
+    动态规划解法
+    状态: 当天结束后是否持股 dp[i][0]/dp[i][1]
+    :param prices:
+    :return:
+    """
+    if not prices: return 0
+    dp = [[0, 0] for _ in range(len(prices))]
+    dp[0][0], dp[0][1] = 0, -prices[0]
+    for i in range(1, len(prices)):
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+    return dp[-1][0]
+print(maxProfit_n_dp([7,1,5,3,6,4]))
+
+
+def maxProfit_2(prices):
+    """
+    限制只能进行两次交易
+    状态选择: 是否持有股票/交易次数
+    :param prices:
+    :return:
+    """
+    if not prices: return 0
+    dp = [[[0, 0, 0], [0, 0, 0]] for _ in range(len(prices))]
+    # dp数组初始化
+    # 第一天不持股
+    dp[0][0][0] = 0
+    dp[0][0][1] = float('-inf')
+    dp[0][0][2] = float('-inf')
+    # 第一天持股
+    dp[0][1][0] = -prices[0]
+    dp[0][1][1] = float('-inf')
+    dp[0][1][2] = float('-inf')
+
+    for i in range(1, len(prices)):
+        # 第i天不持有股票
+        dp[i][0][0] = 0
+        dp[i][0][1] = max(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i])
+        dp[i][0][2] = max(dp[i - 1][0][2], dp[i - 1][1][1] + prices[i])
+
+        # 第i天持有股票
+        dp[i][1][0] = max(dp[i - 1][1][0], dp[i - 1][0][0] - prices[i])
+        dp[i][1][1] = max(dp[i - 1][1][1], dp[i - 1][0][1] - prices[i])
+        dp[i][1][2] = float('-inf')
+    return max(dp[-1][0])
+print(maxProfit_2([3,3,5,0,0,3,1,4]))
